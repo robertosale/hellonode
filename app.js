237a14c99fe30a1dda091fab7322ae11db1c4931
Roberto Sale <roberto.m.sale@gmail.com>
2020-04-15 22:10:09 -0300

    implement routing for contact and about

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ var http = require('http');
 
 http.createServer(function(req,res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("<h1>Hello World con HTML</h1>");
+
+    var url = req.url;
+
+    if(url === '/contact')     res.write("<h1>Contacto<h1>");
+    else if(url === '/about')  res.write("<h1>Acerca de<h1>"); 
+    else                       res.write("<h1>Bienvenido<h1>"); 
     res.end();
+
+  
 }).listen(3000, function(){
     console.log("Servidor escuchando en puerto 30000")
 });
